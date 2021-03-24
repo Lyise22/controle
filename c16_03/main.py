6e61082e99b7009531fd039404b474224747881f
Lyise22 <estelle.li@etu.u-paris.fr>
2021-03-24 17:36:16 +0000

    Commit

--- c16_03/main.py
+++ c16_03/main.py
@@ -21,28 +21,29 @@ moyenne_maths_eleve1 = notes_maths_eleve1/3
 print("La moyenne en maths de l'elève 1 est de",moyenne_maths_eleve1)
 
 # 4.c
-def moyenne_tuples(notes, eleve, matiere):
-  res = [item for item in notes if item[0] == eleve]
-  res1 = [item for item in notes if item[1] == matiere]
+def moyenne_tuples(notes, eleve = None, matiere = None):
+  res = [item for item in notes if item[0] == eleve] if eleve is not None else notes
+  res1 = [item1 for item1 in res if item1[1] == matiere] if matiere is not None else res
   print(res)
   print(res1)
   return sum([x[2] for x in res1])/(len(res1))
 
-print("La moyenne 1 est",moyenne_tuples(notes, "eleve1","math"))
-print("La moyenne 2 est",moyenne_tuples(notes, "eleve1","eco"))
-print("La moyenne 3 est",moyenne_tuples(notes, "eleve2","math"))
+print("La moyenne de l'élève 1 en maths est",moyenne_tuples(notes, "eleve1","math"))
+print("La moyenne de l'élève 1 en eco est",moyenne_tuples(notes, "eleve1","eco"))
+print("La moyenne de l'élève 2 en maths est",moyenne_tuples(notes, "eleve2","math"))
 
 #### 
+# 5.
+n = []
 class Note:
   def __init__(self, eleve, matiere, valeur): #La méthode pour créer un objet
     self.eleve = eleve
     self.matiere = matiere
     self.valeur = valeur
-
+    self = n.append(self)
 
   def afficher(self):
-    print('eleve', self.eleve, 'matiere', self.matiere, 'note', self.valeur)
-
+    return print('eleve :', self.eleve, ', matiere :', self.matiere, ', note :', self.valeur)
 
 onote = Note('eleve1', 'maths', 13)
 print(onote.eleve)
@@ -50,7 +51,61 @@ print(onote.matiere)
 print(onote.valeur)
 Note.afficher(onote)
 
+onotes = []
+question5 = [n1 for n1 in notes if onotes.append(Note(n1[0],n1[1],n1[2]))]
+print(question5)
+
+# 6. 
 def __str__(self):
-  return print(Note) #ce que vous voulez afficher"
+  return print(onote)
+    
+# 7.
+
+
+# 8.
+def moyenne_Notes(eleve = None, matiere = None):
+  maliste = n
+  b = []
+  maliste1 = []
+  for i in maliste :
+    maliste1 = [x for x in maliste if x.eleve == eleve or eleve == None]
+    maliste_matiere = [x for x in maliste1 if x.matiere == matiere or matiere == None]
+    b = [x.valeur for x in maliste_matiere]
+    moy = sum(b)/len(b)
+    return moy
+
+print("La moyenne des Notes est", moyenne_Notes())
+
+###
+
+class Demo:
+  classattr = 'defaut'
+  def __init__(self, a):
+    self.a = a
+
+
+demo1 = Demo(1)
+demo2 = Demo(2)
+
+print(demo1.a)
+print(demo2.a)
+print(Demo.classattr)
+print(demo1.classattr)
+print(demo2.classattr)
+
+Demo.classattr = 23
+
+print(demo1.classattr)
+print(demo2.classattr)
+
+demo1.classattr = -1
+
+print(Demo.classattr)
+print(demo1.classattr)
+print(demo2.classattr)
+
+Demo.classattr = 14
+
+
 
 
